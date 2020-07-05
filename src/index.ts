5f30e3b35bc274be0faaed0ab051de994a41539c
sunchi1015 <67795465+sunchi1015@users.noreply.github.com>
2020-07-05 17:28:31 +0530

    added rxjs beginners

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
-import { Vue } from "../src/vueclass-in-typscript";
+// import { Vue } from "../src/vueclass-in-typscript";
+import { RXJSPractise } from "../src/rxjs-beginner";
+
+// rxjs beginner different operators
+new RXJSPractise();
 
 // module code Vue class clalled here
-new Vue({
-  el: "#app",
-  data: {
-    message: "Hello Vue!",
-    age: 23
-  }
-});
+// new Vue({
+//   el: "#app",
+//   data: {
+//     message: "Hello Vue!",
+//     age: 23
+//   }
+// });
---
+++ src/rxjs-beginner.ts
@@ -0,0 +1,24 @@
+import "./style.css";
+import { of, Observable } from "rxjs";
+import { map, tap, filter } from "rxjs/operators";
+
+export class RXJSPractise {
+  pokerman$: Observable<string>;
+
+  constructor() {
+    this.pokerman$ = of("water", "beer");
+    this.init();
+  }
+
+  private init(): void {
+    this.pokerman$
+      .pipe(
+        filter(type => type === "water"),
+        map(add => "got poker " + add),
+        tap(log => console.log("poker log ", log))
+      )
+      .subscribe(finalData => {
+        console.log("save data to poker db ", finalData);
+      });
+  }
+}
