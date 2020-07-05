6fb5a9de27f43c657b4d8ccda3a3fb7a655cde5f
sunchi1015 <67795465+sunchi1015@users.noreply.github.com>
2020-07-05 17:22:09 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { Vue } from "../src/vueclass-in-typscript";
+
+// module code Vue class clalled here
+new Vue({
+  el: "#app",
+  data: {
+    message: "Hello Vue!",
+    age: 23
+  }
+});
---
+++ src/vueclass-in-typscript.ts
@@ -0,0 +1,15 @@
+import "./style.css";
+
+// Vue js class created in plain javascript
+export class Vue {
+  constructor(options: any) {
+    const el = document.querySelector(options.el);
+    const data = options.data;
+
+    Object.keys(data).forEach(key => {
+      console.log(el);
+      el.innerHTML = el.innerHTML.replace(`{{ ${key} }}`, data[key]);
+    });
+    console.log(el);
+  }
+}
